cb250a1fccf22e9994e3cbdf1e8e713ea073dd53
estebanrm1 <solsel90@gmail.com>
2023-06-01 11:15:30 -0300

    Se subio el ejercicio 1 al git

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Titulo from './components/Titulo'
+
+function App() {
+  
+
+  return (
+    <>
+      <Titulo></Titulo>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+const Titulo = () => {
+    return (
+        <section>
+            <h1>Hello world!</h1>
+        </section>
+    );
+};
+
+export default Titulo;
